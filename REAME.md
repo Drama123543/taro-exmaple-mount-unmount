0d4f9145ed39be1c512858013ff1d8ce1b843219
zhangdonghua <zhangdonghua@967111.com>
2019-10-31 14:37:55 +0800

    测试代码

---
+++ REAME.md
@@ -0,0 +1,5 @@
+有个需求要多次 mount,unmouted
+
+1. npm install
+2. npm run dev:h5
+3. 调式工具多次调用 unmounted、mount，mount 无效
---
+++ src/app.jsx
@@ -0,0 +1,49 @@
+import Taro, { Component } from "@tarojs/taro";
+import Index from "./pages/index";
+
+import "./app.css";
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+class App extends Component {
+  config = {
+    pages: ["pages/index/index"],
+    window: {
+      backgroundTextStyle: "light",
+      navigationBarBackgroundColor: "#fff",
+      navigationBarTitleText: "WeChat",
+      navigationBarTextStyle: "black"
+    }
+  };
+
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  componentDidCatchError() {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return <Index />;
+  }
+}
+
+export const mount = async () => {
+  console.log("mount");
+  Taro.render(<App />, document.getElementById("app"));
+};
+
+export async function unmount() {
+  console.log("unmount");
+  Taro.unmountComponentAtNode(document.getElementById("app"));
+}
+
+window.mount = mount;
+window.unmount = unmount;
